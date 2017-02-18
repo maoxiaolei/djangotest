6281edb557f9ee428c5e8096c9b19bab486c5ff3
maoxiaolei <homemole@163.com>
2017-02-18 22:48:21 +0800

    oderby

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('+pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
