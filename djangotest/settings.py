1a432f2df85e0f22d2dff061ae5898687b49003b
maoxiaolei <homemole@163.com>
2017-02-22 12:58:51 +0800

    template

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -105,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
+
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DATETIME_FORMAT = 'Y-m-d H:i:s'
--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Question,Choice
+import datetime
 # Register your models here.
 
 class ChoiceInline(admin.TabularInline):
@@ -13,5 +14,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
